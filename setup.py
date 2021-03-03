0e7131abf085be6ea8d825eb29b5fa9379bb16e0
rieutordt <thomas.rieutord@meteo.fr>
2021-03-03 09:44:45 +0100

    Rename the package : blcovid -> blusc

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="blcovid",
-    version="0.3.0",
+    name="blusc",
+    version="0.4.0",
     author="Meteo-France",
     author_email="thomas.rieutord@meteo.fr",
-    description="Boundary layer classification made during Covid-19 outbreak",
+    description="Boundary layer unsupervised and supervised classification",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ThomasRieutord/bl-classification",
